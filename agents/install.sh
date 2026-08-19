a8819f5560bb3490245bee1d5ce774a584f9f29a
Jarrett Gilliam <jarrettgilliam@gmail.com>
2026-08-19 10:35:49 -0500

    Add some mattpocock skills

--- agents/install.sh
+++ agents/install.sh
@@ -28,4 +28,16 @@ install_shared ".claude/CLAUDE.md"                ".claude/skills"
 install_shared ".copilot/copilot-instructions.md" ".copilot/skills"
 install_shared ".codex/AGENTS.md"                 ".codex/skills"
 
+for x in \
+    "engineering/improve-codebase-architecture" \
+    "engineering/codebase-design" \
+    "engineering/domain-modeling" \
+    "productivity/grill-me" \
+    "productivity/grilling"
+do
+    link_home "$PKG_DIR/shared/mattpocock_skills/skills/$x/" ".claude/skills/$(basename "$x")"
+    link_home "$PKG_DIR/shared/mattpocock_skills/skills/$x/" ".copilot/skills/$(basename "$x")"
+    link_home "$PKG_DIR/shared/mattpocock_skills/skills/$x/" ".codex/skills/$(basename "$x")"
+done
+
 exit "$DOTFILES_FAILED"
